90fd90cec2670d9d4edead1419fc70f01943bab0
unknown <robertovirayii@gmail.com>
2020-04-29 20:31:49 +0800

    Updates

---
+++ precache-manifest.cf66f28e9a9430440e77969a41ffed94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f59c5055a0afbba889b434334dc759",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "8c08ae7e3e644e70724c",
+    "url": "/react-practice/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "78a3192b7bc94ec8510b",
+    "url": "/react-practice/static/js/2.61910a10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.61910a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c08ae7e3e644e70724c",
+    "url": "/react-practice/static/js/main.eacc69f9.chunk.js"
+  },
+  {
+    "revision": "04682e10a173023a135b",
+    "url": "/react-practice/static/js/runtime-main.57bb6f5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
